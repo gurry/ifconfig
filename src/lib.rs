ad7786e10cefdffc32b7476df8fab23c7cc2f592
Gurinder Singh <frederick.the.fool@gmail.com>
2017-12-10 12:17:09 +0530

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,39 @@
 
 #[macro_use] extern crate foreign_types;
 #[macro_use] extern crate failure;
-extern crate winapi;
+#[macro_use] extern crate bitflags;
 
-
-use foreign_types::{ForeignType, ForeignTypeRef};
 use std::io;
 use std::ptr;
 use std::ffi::CStr;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use failure::Error;
-use winapi::{AF_UNSPEC, ERROR_SUCCESS, ERROR_BUFFER_OVERFLOW, ULONG};
-
 
 
 #[cfg(target_os = "linux")]
 mod linux {
+    // TODO: Eventually remove dependency on crate pnetlink and replace it with some netlink code written right here.
+    // Alternatively, maybe write a properly design netlink crate as well and depend on that.
+    // See Go's package https://github.com/vishvananda/netlink for inspiration
     extern crate libc;
+    extern crate pnetlink;
+    use pnetlink::packet::netlink::NetlinkConnection;
+    use pnetlink::packet::route::link::{Links,Link};
+    use pnetlink::packet::route::addr::{Addresses,Addr};
+
+    let mut conn = NetlinkConnection::new();
+    let links = conn.iter_links().unwrap().collect::<Vec<_>>();
+    for link in links {
+        print_link(&link);
+        for addr in conn.get_link_addrs(None, &link).unwrap() {
+            //println!("{:?}", addr.get_ip());
+            print_addr(&addr);
+        }
+    }
+
+
+
+
     foreign_type! {
         type CType = libc::ifaddrs;
         fn drop = libc::freeifaddrs;
@@ -136,45 +153,117 @@ mod linux {
             );
         }
     }
+
+    fn get_addrs() -> Result<impl Iterator<Item=IpAddr>, Error> {
+        let mut conn = NetlinkConnection::new();
+        let links = conn.iter_links().unwrap().collect::<Vec<_>>();
+        for link in links {
+            print_link(&link);
+            for addr in conn.get_link_addrs(None, &link).unwrap() {
+                print_addr(&addr);
+            }
+        }
+    }
 }
 
 #[cfg(target_os = "windows")]
 mod windows {
-fn GetAdaptersAddresses ( Family : ULONG , Flags : ULONG , Reserved : PVOID , AdapterAddresses : PIP_ADAPTER_ADDRESSES , SizePointer : PULONG , ) -> ULONG; 
+    extern crate winapi;
+    use winapi::{AF_UNSPEC, ERROR_SUCCESS, ERROR_BUFFER_OVERFLOW, ULONG};
 
-pub fn get_adapters() -> Result<Vec<Adapter>> {
-    unsafe {
-        let mut buf_len: ULONG = 0;
-        let result = GetAdaptersAddresses(AF_UNSPEC as u32, 0, std::ptr::null_mut(), std::ptr::null_mut(), &mut buf_len as *mut ULONG);
+    fn GetAdaptersAddresses ( Family : ULONG , Flags : ULONG , Reserved : PVOID , AdapterAddresses : PIP_ADAPTER_ADDRESSES , SizePointer : PULONG , ) -> ULONG; 
 
-        assert!(result != ERROR_SUCCESS);
+    pub fn get_adapters() -> Result<Vec<Adapter>> {
+        unsafe {
+            let mut buf_len: ULONG = 0;
+            let result = GetAdaptersAddresses(AF_UNSPEC as u32, 0, std::ptr::null_mut(), std::ptr::null_mut(), &mut buf_len as *mut ULONG);
 
-        if result != ERROR_BUFFER_OVERFLOW {
-            bail!(ErrorKind::Os(result));
+            assert!(result != ERROR_SUCCESS);
+
+            if result != ERROR_BUFFER_OVERFLOW {
+                bail!(ErrorKind::Os(result));
+            }
+
+            let mut adapters_addresses_buffer: Vec<u8> = vec![0; buf_len as usize];
+            let mut adapter_addresses_ptr: PIP_ADAPTER_ADDRESSES = std::mem::transmute(adapters_addresses_buffer.as_mut_ptr());
+            let result = GetAdaptersAddresses(AF_UNSPEC as u32, 0, std::ptr::null_mut(), adapter_addresses_ptr, &mut buf_len as *mut ULONG);
+
+            if result != ERROR_SUCCESS {
+                bail!(ErrorKind::Os(result));
+            }
+
+            let mut adapters = vec![];
+            while adapter_addresses_ptr != std::ptr::null_mut() {
+                adapters.push(get_adapter(adapter_addresses_ptr)?);
+                adapter_addresses_ptr = (*adapter_addresses_ptr).Next;
+            }
+
+            Ok(adapters)
+        }
+    }
+
+    unsafe fn get_adapter(adapter_addresses_ptr: PIP_ADAPTER_ADDRESSES) -> Result<Adapter> {
+        let adapter_addresses = &*adapter_addresses_ptr;
+        let adapter_name = CStr::from_ptr(adapter_addresses.AdapterName).to_str()?.to_owned();
+        let dns_servers = get_dns_servers(adapter_addresses.FirstDnsServerAddress)?;
+        let unicast_addresses = get_unicast_addresses(adapter_addresses.FirstUnicastAddress)?;
+
+        let description = WideCString::from_ptr_str(adapter_addresses.Description).to_string()?;
+        let friendly_name = WideCString::from_ptr_str(adapter_addresses.FriendlyName).to_string()?;
+        Ok(Adapter {
+            adapter_name: adapter_name,
+            ip_addresses: unicast_addresses,
+            dns_servers: dns_servers,
+            description: description,
+            friendly_name: friendly_name,
+        })
+    }
+
+    unsafe fn socket_address_to_ipaddr(socket_address: &SOCKET_ADDRESS) -> IpAddr {
+        let sockaddr = socket2::SockAddr::from_raw_parts(std::mem::transmute(socket_address.lpSockaddr), socket_address.iSockaddrLength);
+
+        // Could be either ipv4 or ipv6
+        sockaddr.as_inet()
+            .map(|s| IpAddr::V4(*s.ip()))
+            .unwrap_or_else(|| IpAddr::V6(*sockaddr.as_inet6().unwrap().ip()))
+    }
+
+    unsafe fn get_dns_servers(mut dns_server_ptr: PIP_ADAPTER_DNS_SERVER_ADDRESS_XP) -> Result<Vec<IpAddr>> {
+        let mut dns_servers = vec![];
+
+        while dns_server_ptr != std::ptr::null_mut() {
+            let dns_server = &*dns_server_ptr;
+            let ipaddr = socket_address_to_ipaddr(&dns_server.Address);
+            dns_servers.push(ipaddr);
+
+            dns_server_ptr = dns_server.Next;
         }
 
-        let mut adapters_addresses_buffer: Vec<u8> = vec![0; buf_len as usize];
-        let mut adapter_addresses_ptr: PIP_ADAPTER_ADDRESSES = std::mem::transmute(adapters_addresses_buffer.as_mut_ptr());
-        let result = GetAdaptersAddresses(AF_UNSPEC as u32, 0, std::ptr::null_mut(), adapter_addresses_ptr, &mut buf_len as *mut ULONG);
+        Ok(dns_servers)
+    }
 
-        if result != ERROR_SUCCESS {
-            bail!(ErrorKind::Os(result));
+    unsafe fn get_unicast_addresses(mut unicast_addresses_ptr: PIP_ADAPTER_UNICAST_ADDRESS_LH) -> Result<Vec<IpAddr>> {
+        let mut unicast_addresses = vec![];
+
+        while unicast_addresses_ptr != std::ptr::null_mut() {
+            let unicast_address = &*unicast_addresses_ptr;
+            let ipaddr = socket_address_to_ipaddr(&unicast_address.Address);
+            unicast_addresses.push(ipaddr);
+
+            unicast_addresses_ptr = unicast_address.Next;
         }
 
-        let mut adapters = vec![];
-        while adapter_addresses_ptr != std::ptr::null_mut() {
-            adapters.push(get_adapter(adapter_addresses_ptr)?);
-            adapter_addresses_ptr = (*adapter_addresses_ptr).Next;
-        }
+        Ok(unicast_addresses)
+    }
 
-        Ok(adapters)
+    fn get_addrs() -> Result<impl Iterator<Item=IpAddr>, Error> {
+        // TODO: Call GetAdapters here or something
+        unimplemented!()
     }
 }
 
-}
-
 // IEEE MAC-48, EUI-48 and EUI-64 form
-struct HardwareAddr(u8[]); // TODO: should we use something other than bytes as the underlying type?
+struct HardwareAddr([u8; 6]); // TODO: should we use something other than bytes as the underlying type? Are 6 bytes enough?
 
 struct Interface {
   	index: u32,
@@ -184,16 +273,54 @@ struct Interface {
   	flags: Flags,
 }
 
-struct Flags(u32); // TODO: should we use u32 as the underlying type?
-  
-  const (
-  	FlagUp           Flags = 1 << iota // interface is up
-  	FlagBroadcast                      // interface supports broadcast access capability
-  	FlagLoopback                       // interface is a loopback interface
-  	FlagPointToPoint                   // interface belongs to a point-to-point link
-  	FlagMulticast                      // interface supports multicast access capability
-  )
+bitflags! {
+    struct Flags: u32 {
+        const FLAG_UP              = 0b00000001;
+        const FLAG_BROADCAST       = 0b00000010;
+        const FLAG_LOOPBACK        = 0b00000100;
+        const FLAG_POINTTOPOINT    = 0b00001000;
+        const FLAG_MULTICAST       = 0b00010000;
+    }
+}
 
+// Returns a list of the system's network interfaces.
 pub fn get_interfaces() -> Result<impl Iterator<Item=&Interface>, Error> {
     Ok(Vec::new().iter())
 }
+
+// Returns a list of the system's unicast interface addresses.
+//
+// The returned list does not identify the associated interface.
+// Use get_interfaces and Interface.addrs() for more detail.
+pub fn get_addrs() -> Result<impl Iterator<Item=IpAddr>, Error> {
+    #[cfg(target_os = "linux")]
+    linux::get_addrs()
+
+    #[cfg(target_os = "windows")]
+    windows::get_addrs()
+}
+
+// Corresponding functions from golang recorded below for reference
+// func Interfaces() ([]Interface, error) {
+// 	ift, err := interfaceTable(0)
+// 	if err != nil {
+// 		return nil, &OpError{Op: "route", Net: "ip+net", Source: nil, Addr: nil, Err: err}
+// 	}
+// 	if len(ift) != 0 {
+// 		zoneCache.update(ift)
+// 	}
+// 	return ift, nil
+// }
+
+// // InterfaceAddrs returns a list of the system's unicast interface
+// // addresses.
+// //
+// // The returned list does not identify the associated interface; use
+// // Interfaces and Interface.Addrs for more detail.
+// func InterfaceAddrs() ([]Addr, error) {
+// 	ifat, err := interfaceAddrTable(nil)
+// 	if err != nil {
+// 		err = &OpError{Op: "route", Net: "ip+net", Source: nil, Addr: nil, Err: err}
+// 	}
+// 	return ifat, err
+// }
